2f01d5d6b5950f93a2520c035cae2611a6487fa8
zhangpeng <zhangpeng.0304@aliyun.com>
2019-10-29 10:18:08 +0800

    log

--- cmd/json.go
+++ cmd/json.go
@@ -62,14 +62,4 @@ var jsonCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(jsonCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// jsonCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// jsonCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ cmd/log.go
@@ -0,0 +1,52 @@
+// Copyright © 2019 zhangpeng zhangpeng.0304@aliyun.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"net/http"
+)
+
+var log = &cobra.Command{
+	Use:   "log",
+	Short: "获取日志",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("请输入手机号：")
+		var phone string
+		_, _ = fmt.Scanln(&phone)
+		fmt.Println("请输入日期，例：20190807：")
+		var date string
+		_, _ = fmt.Scanln(&date)
+		fmt.Println("请输入平台，例：ios/android/windows：")
+		var platform string
+		_, _ = fmt.Scanln(&platform)
+		fmt.Println("-----------------------------")
+		resp, err := http.Get("https://hx.zuoyebang.com/flipped-classin/loginfo/getlivelog?phone=15900242911&date=20190724&os=ios")
+		if err != nil {
+			// handle error
+		}
+		//fmt.Println(resp.Body)
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(body)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(log)
+}
